c9e06f6837038c69c320018ab8e60f68e0715d34
Nedelin Petkov <mlg@abv.bg>
2019-08-12 20:32:42 +0300

    Update basic_func.sh

--- basic_func.sh
+++ basic_func.sh
@@ -2,7 +2,7 @@
 #
 # Description: Basic funtions for all recipes
 # Author: Nedelin Petkov
-# Version: 0.3
+# Version: 0.4
 #
 # Exit codes:
 # 0 - OK
@@ -10,7 +10,7 @@
 
 # Function for output logging
 # Example:
-# log "ERROR" "Installing httpd"
+#   log "ERROR" "Installing httpd"
 function log {
 	LEVEL=$1
 	MESSAGE=$2
@@ -19,39 +19,49 @@ function log {
 
 # Function for check exit codes after every command
 # Example:
-# /bin/yum install -y -q httpd
-# check_exit_code $? "Install httpd"
+#   /bin/yum install -y -q httpd
+#   check_exit_code $? "Install httpd"
 function check_exit_code {
 	CODE=$1
 	MESSAGE=$2
         if [ $CODE -ne 0 ]; then
 		log "ERROR" "$MESSAGE"
-                exit 1
-        else
+		exit 1
+	else
 		log "INFO" "$MESSAGE"
         fi
 }
 
-
 # Function for requiring package
+# Usage:
+#   require packages_name install_packages_yes_or_no__default_yes
 # Example:
-# require wget
+#   require wget
+#   require httpd no
 function require {
         REQU=$1
-        /bin/rpm -qa | grep $REQU > /dev/null 2>&1
-        if [ $? -ne 0 ]; then
+	INSTALL=$2
+	if [ "$INSTALL" = "" ]; then
+		INSTALL="yes"
+	fi
+	/bin/rpm -qa | grep $REQU > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
 		log "WARN" "$REQU is not installed"
-                /bin/yum install -y -q $REQU > /dev/null 2>&1
-                check_exit_code $? "Installing '$REQU'"
+		if [ "$INSTALL" = "yes" ]; then
+			/bin/yum install -y -q $REQU > /dev/null 2>&1
+			check_exit_code $? "Installing '$REQU'"
+		else
+			log "ERROR" "$REQU is not installed"
+			exit 1
+		fi
 	else
 		log "INFO" "$REQU is installed"
         fi
 }
 
-
 # Function for password generating
 # Example:
-# password 16
+#   password 16
 function password {
 	LENGTH=$1
 	date +"%s" | sha256sum | base64 -w 0 | head -c $LENGTH
