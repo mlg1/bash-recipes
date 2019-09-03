fc80815f4d5861b36ee740307ad0a418e92268e8
Nedelin Petkov <mlg@abv.bg>
2019-09-03 22:25:47 +0300

    Add firewalld services to httpd installers

--- install_httpd-2.4-centos.sh
+++ install_httpd-2.4-centos.sh
@@ -13,6 +13,7 @@ source basic_func.sh
 
 # Require list
 require epel-release
+require firewalld no
 
 # Installing the necessary packages
 /bin/yum install -y -q httpd mod_ssl mod_fcgid > $0.log 2>&1
@@ -24,5 +25,13 @@ check_exit_code $? "Start 'httpd' service"
 
 /usr/bin/systemctl enable httpd > $0.log 2>&1
 check_exit_code $? "Enable to start on boot 'httpd' service"
+
+# Add http and https service in Firewalld
+/usr/bin/firewall-cmd --zone=public --add-service=http > $0.log 2>&1
+/usr/bin/firewall-cmd --zone=public --permanent --add-service=http > $0.log 2>&1
+check_exit_code $? "Add 'http' service in Firewalld"
+/usr/bin/firewall-cmd --zone=public --add-service=https > $0.log 2>&1
+/usr/bin/firewall-cmd --zone=public --permanent --add-service=https > $0.log 2>&1
+check_exit_code $? "Add 'https' service in Firewalld"
 
 log "INFO" "Done"
--- install_httpd-2.4-codeit.sh
+++ install_httpd-2.4-codeit.sh
@@ -14,6 +14,7 @@ source basic_func.sh
 # Require list
 require epel-release
 require wget
+require firewalld no
 
 # Download CodeIT repo
 /usr/bin/wget https://repo.codeit.guru/codeit.el7.repo -P /etc/yum.repos.d/ > $0.log 2>&1
@@ -29,5 +30,13 @@ check_exit_code $? "Start 'httpd' service"
 
 /usr/bin/systemctl enable httpd > $0.log 2>&1
 check_exit_code $? "Enable to start on boot 'httpd' service"
+
+# Add http and https service in Firewalld
+/usr/bin/firewall-cmd --zone=public --add-service=http > $0.log 2>&1
+/usr/bin/firewall-cmd --zone=public --permanent --add-service=http > $0.log 2>&1
+check_exit_code $? "Add 'http' service in Firewalld"
+/usr/bin/firewall-cmd --zone=public --add-service=https > $0.log 2>&1
+/usr/bin/firewall-cmd --zone=public --permanent --add-service=https > $0.log 2>&1
+check_exit_code $? "Add 'https' service in Firewalld"
 
 log "INFO" "Done"
